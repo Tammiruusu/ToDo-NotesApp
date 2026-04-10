a833de246601f55827a409c4a2fa86bd9fb9f926
Keyfr4mes <tiiu_maria@hotmail.com>
2026-04-10 11:34:13 +0300

    Incorrect luokka liitetty kaikkiin input kenttiin, Error rekisteröinnissä tehty valmiiksi

--- Js/validation.js
+++ Js/validation.js
@@ -7,7 +7,7 @@ const repeat_password_input = document.getElementById('repeat-password-input')
 
 form.addEventListener('submit', (e) => {
     //estetään lomakkeen submittaus, jos siinä on virhe
-    //e.preventDefault() 
+    //Alla on lista, joka ilmoittaa virheistä ja virhe ilmoituksista
     let errors = []
 
     //rekisteröitymis sivun ja sisäänkirjautumis sivulla eri virheet
@@ -25,4 +25,43 @@ form.addEventListener('submit', (e) => {
         errors = getLoginFormErrors(email_input.value, password_input)
     }
 
+    if(errors.length > 0){
+        //estetään lomakkeen lähetys, jos Errors listalla on yhtäkään virhettä
+        //niin kauan kuin se on suurempi kuin nolla, niin lomaketta ei voi lähettää
+        e.preventDefault()
+    }
 })
+
+
+function getSignupFormErrors(firstname, lastname, email, password, repeatPassword) {
+    let errors = []
+
+    if(firstname === '' || firstname === null) {
+        errors.push('Tarvitaan Etunimi')
+        //virhe ilmoitus pitää tuoda näkyviin käyttäjälle
+        firstname_input.parentElement.classList.add('incorrect')
+    }
+
+    
+    if(lastname === '' || lastname === null) {
+        errors.push('Tarvitaan Sukunimi')
+        //virhe ilmoitus pitää tuoda näkyviin käyttäjälle
+        lastname_input.parentElement.classList.add('incorrect')
+    }
+
+    
+    if(email === '' || email === null) {
+        errors.push('Tarvitaan Sähköposti osoite')
+        //virhe ilmoitus pitää tuoda näkyviin käyttäjälle
+        email_input.parentElement.classList.add('incorrect')
+    }
+
+    
+    if(password === '' || password === null) {
+        errors.push('Tarvitaan Salasana')
+        //virhe ilmoitus pitää tuoda näkyviin käyttäjälle
+        password_input.parentElement.classList.add('incorrect')
+    }
+
+    return errors;
+}
